c3056e80bca8fb1e9ff26ee5d68e879164a0bcc4
kwaiboo <413361089@QQ.com>
2016-10-17 11:55:36 +0800

    Update DoubanPicDownload.py

--- DoubanPicDownload.py
+++ DoubanPicDownload.py
@@ -11,6 +11,14 @@ def fileName(path):
     picPath=os.path.join(localPath,path[pos+1:])
     return picPath
 
+def saveFile(data):
+    savePath='C:\\kankan\\temp.txt'
+    if not os.path.exists(savePath):
+        os.mkdir(savePath)
+    file=open(savePath,'wb')
+    file.write(data)
+    file.close()
+
 if __name__=='__main__':
     url='https://music.douban.com/'   #抓取的是豆瓣音乐页面上的所有图片，更改网址后可以抓取其他网页上的页面
     headers={
@@ -23,6 +31,15 @@ if __name__=='__main__':
         }
     req=urllib.request.Request(url=url,headers=headers)
     html=urllib.request.urlopen(req)
+    
+    Page=html.read()
+    savePage=Page.decode('utf-8')
+
+    print(type(html))             #<class 'http.client.HTTPResponse'>
+    print(type(Page))             #<class 'bytes'>
+    print(type(savePage))         #<class 'str'>
+    saveFile(Page)
+    
     bs=BeautifulSoup(markup=html,features='html.parser')
     for link in bs.findAll('img'):
         print(link['src'])
